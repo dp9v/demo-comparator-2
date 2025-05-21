69497185b30037f05e970e50b19461415d9196b2
dpolkovnikov <polkovnikov13@gmail.com>
2025-05-21 17:40:52 +0200

    Add CLI

--- app/src/main/java/io/hexlet/Application.java
+++ app/src/main/java/io/hexlet/Application.java
@@ -1,7 +1,36 @@
 package io.hexlet;
 
-public class Application {
+import picocli.CommandLine;
+
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name="app", mixinStandardHelpOptions = true, version = "1.0")
+public class Application implements Callable<Integer> {
+
+    @CommandLine.Parameters(index = "0", description = "The first file to be merged.", paramLabel = "filepath1")
+    private String file1;
+
+    @CommandLine.Parameters(index = "1", description = "The first file to be merged.", paramLabel = "filepath2")
+    private String file2;
+
+    @CommandLine.Option(
+            names = {"-f", "--format"},
+            paramLabel = "format",
+            description = "output format [default: stylish]",
+            defaultValue = "stylish"
+    )
+    private String format;
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        var res = new CommandLine(new Application()).execute(args);
+        System.exit(res);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        var diff = Differ.generate(file1, file2);
+        System.out.println(diff);
+        return 0;
     }
 }
---
+++ app/src/main/java/io/hexlet/Differ.java
@@ -0,0 +1,7 @@
+package io.hexlet;
+
+public class Differ {
+    public static String generate(String file1, String file2) {
+        return "{}";
+    }
+}
